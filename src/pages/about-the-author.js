7a8a2d252f1c7149f0dd45b1d91ca6fba5a91e29
inksharedsolutions <57968903+inksharedsolutions@users.noreply.github.com>
2022-02-17 01:13:34 +0800

    Update about-the-author.js

--- src/pages/about-the-author.js
+++ src/pages/about-the-author.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import Nav from '../components/nav'
 import Banner from '../components/non-front-banner'
-import AuthorImg from '../../static/author/main_author.png'
+import AuthorImg from '../../static/author/author.jpeg'
 import { Helmet } from 'react-helmet'
 
 const ATB = (props) => {
