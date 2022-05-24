c51aa5affee7440f0bf4c4983cb96490d3789135
Davie <webdev.inkshared@gmail.com>
2022-05-25 06:43:24 +0800

    added audioplayer

--- src/components/front-book.js
+++ src/components/front-book.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'gatsby'
 import FrontBook from '../../static/books/Book_Cover.png'
+
 // import Accordions from '../containers/accordions'
 
 const MidBook = (props)=>{
@@ -13,6 +14,7 @@ const MidBook = (props)=>{
 
                         <div className="book-wraps">
                             <img src={FrontBook} alt="The Black Tide"/>
+                           
                         </div>
 
                         <div className="content-wraps">
